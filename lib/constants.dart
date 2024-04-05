af143d8b0686a87bfbd615511e9dd4efcdb22bdc
Dorian NUNEZ <dorian@wecrea.com>
2024-04-05 13:29:13 +0200

    Add constant colors

---
+++ lib/constants.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+const Color lightSteelBlue_50 = Color.fromRGBO(193, 217, 251, 1);
+const Color lightSteelBlue_100 = Color.fromRGBO(173, 197, 231, 1);
+const Color lightSteelBlue_200 = Color.fromRGBO(153, 177, 211, 1);
+const Color lightSteelBlue_300 = Color.fromRGBO(133, 157, 191, 1);
+const Color lightSteelBlue_400 = Color.fromRGBO(113, 137, 171, 1);
+const Color lightSteelBlue_500 = Color.fromRGBO(93, 117, 151, 1);
+const Color lightSteelBlue_600 = Color.fromRGBO(83, 107, 141, 1);
+const Color lightSteelBlue_700 = Color.fromRGBO(73, 97, 131, 1);
+const Color lightSteelBlue_800 = Color.fromRGBO(63, 87, 121, 1);
+const Color lightSteelBlue_900 = Color.fromRGBO(43, 67, 101, 1);
+
+
+const Color nonPhotoBlue_50 = Color.fromRGBO(224, 236, 248,1);
+const Color nonPhotoBlue_100 = Color.fromRGBO(168, 219, 238,1);
+const Color nonPhotoBlue_200 = Color.fromRGBO(112, 202, 228,1);
+const Color nonPhotoBlue_300 = Color.fromRGBO(56, 185, 218,1);
+const Color nonPhotoBlue_400 = Color.fromRGBO(0, 168, 208,1);
+const Color nonPhotoBlue_500 = Color.fromRGBO(0, 151, 198,1);
+const Color nonPhotoBlue_600 = Color.fromRGBO(0, 134, 188,1);
+const Color nonPhotoBlue_700 = Color.fromRGBO(0, 117, 178,1);
+const Color nonPhotoBlue_800 = Color.fromRGBO(0, 100, 168,1);
+const Color nonPhotoBlue_900 = Color.fromRGBO(0, 83, 158,1);
+
+const Color water_50 = Color.fromRGBO(224, 243, 248,1);
+const Color water_100 = Color.fromRGBO(212, 241, 237,1);
+const Color water_200 = Color.fromRGBO(200, 239, 226,1);
+const Color water_300 = Color.fromRGBO(188, 237, 215,1);
+const Color water_400 = Color.fromRGBO(176, 235, 204,1);
+const Color water_500 = Color.fromRGBO(164, 233, 193,1);
+const Color water_600 = Color.fromRGBO(152, 231, 182,1);
+const Color water_700 = Color.fromRGBO(140, 229, 171,1);
+const Color water_800 = Color.fromRGBO(128, 227, 160,1);
+const Color water_900 = Color.fromRGBO(116, 225, 149,1);
+
+const Color champagePink_50 = Color.fromRGBO(254, 235, 219,1);
+const Color champagePink_100 = Color.fromRGBO(244, 225, 209,1);
+const Color champagePink_200 = Color.fromRGBO(234, 215, 199,1);
+const Color champagePink_300 = Color.fromRGBO(224, 205, 189,1);
+const Color champagePink_400 = Color.fromRGBO(214, 195, 179,1);
+const Color champagePink_500 = Color.fromRGBO(204, 185, 169,1);
+const Color champagePink_600 = Color.fromRGBO(194, 175, 159,1);
+const Color champagePink_700 = Color.fromRGBO(184, 165, 149,1);
+const Color champagePink_800 = Color.fromRGBO(174, 155, 139,1);
+const Color champagePink_900 = Color.fromRGBO(164, 145, 129,1);
+
+
+const Color dutchWhite_50 = Color.fromRGBO(251, 221, 195,1);
+const Color dutchWhite_100 = Color.fromRGBO(241, 211, 185,1);
+const Color dutchWhite_200 = Color.fromRGBO(231, 201, 175,1);
+const Color dutchWhite_300 = Color.fromRGBO(221, 191, 165,1);
+const Color dutchWhite_400 = Color.fromRGBO(211, 181, 155,1);
+const Color dutchWhite_500 = Color.fromRGBO(201, 171, 145,1);
+const Color dutchWhite_600 = Color.fromRGBO(191, 161, 135,1);
+const Color dutchWhite_700 = Color.fromRGBO(181, 151, 125,1);
+const Color dutchWhite_800 = Color.fromRGBO(171, 141, 115,1);
+const Color dutchWhite_900 = Color.fromRGBO(161, 131, 105,1);
